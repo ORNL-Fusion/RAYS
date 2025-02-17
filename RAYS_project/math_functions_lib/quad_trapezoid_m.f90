22a0630375d56bd9e7afb6841b7829c8f10d3bdc
Donald Batchelor <batchelordb@ornl.gov>
2025-02-17 14:20:04 -0500

    Add stuff for cumulative integral to quad_trapezoid_m

--- RAYS_project/math_functions_lib/quad_trapezoid_m.f90
+++ RAYS_project/math_functions_lib/quad_trapezoid_m.f90
@@ -1,9 +1,13 @@
  MODULE trapezoid_quad_m
 
-! generic procedure: trapezoid_quad to calculate integral from a vector of y
+! generic procedure: trapezoid_quad, calculates integral from a vector of y
 ! values, y(:), using simple trapezoidal approximation
-!    
-! Input x is either a fixed increment, h, or a vector of x(:) values (not necessarily 
+
+! generic procedure: trapezoid_quad_cumulative, calculates integral from a vector of y
+! values, y(:), using simple trapezoidal approximation and also returns the cumulative
+! integral at each grid point in a vector, cum(:) of length size(y).
+
+! Input x is either a fixed increment, h, or a vector of x(:) values (not necessarily
 ! evenly spaced). x,y vector value can be real or complex, single precision or kind = rkind.
 
     IMPLICIT NONE
@@ -15,171 +19,241 @@
         module procedure h_yvec_real, h_yvec_D, h_yvec_cmplx, h_yvec_cmplxD,&
                        & xvec_yvec_real, xvec_yvec_realD, xvec_yvec_cmplx, xvec_yvec_cmplxD
     end interface
-    
+
+    interface trapezoid_quad_cumulative
+        module procedure h_yvec_cum_real, h_yvec_cum_D,&
+                       & xvec_yvec_cum_real, xvec_yvec_cum_D
+    end interface
+
     CONTAINS
  !*********************************************************************************
 
     SUBROUTINE h_yvec_real(h, y, integral)
-    
+
         IMPLICIT NONE
         REAL(kind = skind), INTENT(IN) :: h, y(:)
         REAL(kind = skind), INTENT(out) :: integral
-    
+
         integral = h*(SUM(y(2:SIZE(y))) + y(1)/2. - y(SIZE(y))/2.)
-    
+
     RETURN
     END SUBROUTINE h_yvec_real
  !*********************************************************************************
-    
+
     SUBROUTINE h_yvec_D(h, y, integral)
-    
+
         IMPLICIT NONE
         REAL(kind = rkind), INTENT(IN) :: h, y(:)
         REAL(kind = rkind), INTENT(out) :: integral
-    
+
         integral = h*(SUM(y(2:SIZE(y))) + y(1)/2. - y(SIZE(y))/2.)
-    
+
     RETURN
     END SUBROUTINE h_yvec_D
  !*********************************************************************************
-    
-    
+
+
     SUBROUTINE h_yvec_cmplx(h, y, integral)
-    
+
         IMPLICIT NONE
         REAL(kind = skind), INTENT(IN) :: h
         COMPLEX(kind = skind), INTENT(IN) :: y(:)
         COMPLEX(kind = skind), INTENT(out) :: integral
-    
+
         integral = h*(SUM(y(2:SIZE(y))) + y(1)/2. - y(SIZE(y))/2.)
-    
+
     RETURN
     END SUBROUTINE h_yvec_cmplx
  !*********************************************************************************
-    
+
     SUBROUTINE h_yvec_cmplxD(h, y, integral)
-    
+
         IMPLICIT NONE
         REAL(kind = rkind), INTENT(IN) :: h
         COMPLEX(kind = rkind), INTENT(IN) :: y(:)
         COMPLEX(kind = rkind), INTENT(out) :: integral
-    
+
         integral = h*(SUM(y(2:SIZE(y))) + y(1)/2. - y(SIZE(y))/2.)
-    
+
     RETURN
     END SUBROUTINE h_yvec_cmplxD
  !*********************************************************************************
 
     SUBROUTINE xvec_yvec_real(x, y, integral)
-    
+
         IMPLICIT NONE
         REAL(kind = skind), INTENT(IN) :: x(:), y(:)
         REAL(kind = skind), INTENT(out) :: integral
         integer :: i
-        
+
         if (size(x) /= size(y)) then
             write(*,*) 'quad_trapezoid: size(x) != size(y)'
             stop
         end if
-        
+
         integral = 0.
         do i = 1, size(x)-1
         integral = integral + (x(i+1)-x(i))*(y(i+1)+y(i))
-        end do        
+        end do
         integral = integral/2.
- 
+
     RETURN
     END SUBROUTINE xvec_yvec_real
  !*********************************************************************************
 
     SUBROUTINE xvec_yvec_realD(x, y, integral)
-    
+
         IMPLICIT NONE
         REAL(kind = rkind), INTENT(IN) :: x(:), y(:)
         REAL(kind = rkind), INTENT(out) :: integral
         integer :: i
-        
+
         if (size(x) /= size(y)) then
             write(*,*) 'quad_trapezoid: size(x) != size(y)'
             stop
         end if
-        
+
         integral = 0.
         do i = 1, size(x)-1
         integral = integral + (x(i+1)-x(i))*(y(i+1)+y(i))
-        end do        
+        end do
         integral = integral/2.
- 
+
     RETURN
     END SUBROUTINE xvec_yvec_realD
  !*********************************************************************************
 
     SUBROUTINE xvec_yvec_cmplx(x, y, integral)
-    
+
         IMPLICIT NONE
         REAL(kind = skind), INTENT(IN) :: x(:)
         complex(kind = skind), INTENT(IN) :: y(:)
         complex(kind = skind), INTENT(out) :: integral
         integer :: i
-        
+
         if (size(x) /= size(y)) then
             write(*,*) 'quad_trapezoid: size(x) != size(y)'
             stop
         end if
-        
+
         integral = 0.
         do i = 1, size(x)-1
         integral = integral + (x(i+1)-x(i))*(y(i+1)+y(i))
-        end do        
+        end do
         integral = integral/2.
- 
+
     RETURN
     END SUBROUTINE xvec_yvec_cmplx
  !*********************************************************************************
 
     SUBROUTINE xvec_yvec_cmplxD(x, y, integral)
-    
+
         IMPLICIT NONE
         REAL(kind = rkind), INTENT(IN) :: x(:)
         complex(kind = rkind), INTENT(IN) :: y(:)
         complex(kind = rkind), INTENT(out) :: integral
         integer :: i
-        
+
         if (size(x) /= size(y)) then
             write(*,*) 'quad_trapezoid: size(x) != size(y)'
             stop
         end if
-        
+
         integral = 0.
         do i = 1, size(x)-1
         integral = integral + (x(i+1)-x(i))*(y(i+1)+y(i))
-        end do        
+        end do
         integral = integral/2.
- 
+
     RETURN
     END SUBROUTINE xvec_yvec_cmplxD
  !*********************************************************************************
 
+    SUBROUTINE h_yvec_cum_real(h, y, integral, cum)
 
+    IMPLICIT NONE
+    REAL, INTENT(IN) :: h, y(:)
+    REAL, INTENT(out) :: integral
+    REAL, INTENT(out) :: cum(SIZE(y))
+    INTEGER  :: i
 
-!     
-!     SUBROUTINE trapezoid_h_cum_real(h, y, integral, cum)
-!     
-!     IMPLICIT NONE
-!     REAL, INTENT(IN) :: h, y(:)
-!     REAL, INTENT(out) :: integral
-!     REAL, INTENT(out) :: cum(SIZE(y))
-!     INTEGER  :: i
-!     
-!     cum(1) = 0.
-!     DO i = 2, SIZE(y)
-!         cum(i) = cum(i-1) + (y(i-1) + y(i))/2.
-!     END DO
-!     
-!     cum = h*cum
-!     integral = cum(SIZE(y))
-!     
-!     RETURN
-!     END SUBROUTINE trapezoid_h_cum_real
-        
+    cum(1) = 0.
+    DO i = 2, SIZE(y)
+        cum(i) = cum(i-1) + (y(i-1) + y(i))/2.
+    END DO
+
+    cum = h*cum
+    integral = cum(SIZE(y))
+
+    RETURN
+    END SUBROUTINE h_yvec_cum_real
+ !*********************************************************************************
+
+    SUBROUTINE h_yvec_cum_D(h, y, integral, cum)
+
+    IMPLICIT NONE
+    REAL(kind = rkind), INTENT(IN) :: h, y(:)
+    REAL(kind = rkind), INTENT(out) :: integral
+    REAL(kind = rkind), INTENT(out) :: cum(SIZE(y))
+    INTEGER  :: i
+
+    cum(1) = 0.
+    DO i = 2, SIZE(y)
+        cum(i) = cum(i-1) + (y(i-1) + y(i))/2.
+    END DO
+
+    cum = h*cum
+    integral = cum(SIZE(y))
+
+    RETURN
+    END SUBROUTINE h_yvec_cum_D
+!*********************************************************************************
+
+    SUBROUTINE xvec_yvec_cum_real(x, y, integral, cum)
+
+        IMPLICIT NONE
+        REAL(kind = skind), INTENT(IN) :: x(:), y(:)
+        REAL(kind = skind), INTENT(out) :: integral
+		REAL(kind = skind), INTENT(out) :: cum(SIZE(y))
+        integer :: i
+
+        if (size(x) /= size(y)) then
+            write(*,*) 'quad_trapezoid: size(x) != size(y)'
+            stop
+        end if
+
+		cum(1) = 0.
+		DO i = 2, SIZE(y)
+			cum(i) = cum(i-1) + (x(i)-x(i-1))*(y(i-1) + y(i))/2.
+		END DO
+
+	    integral = cum(SIZE(y))
+
+    RETURN
+    END SUBROUTINE xvec_yvec_cum_real
+!*********************************************************************************
+
+    SUBROUTINE xvec_yvec_cum_D(x, y, integral, cum)
+
+        IMPLICIT NONE
+        REAL(kind = rkind), INTENT(IN) :: x(:), y(:)
+        REAL(kind = rkind), INTENT(out) :: integral
+		REAL(kind = rkind), INTENT(out) :: cum(SIZE(y))
+        integer :: i
+
+        if (size(x) /= size(y)) then
+            write(*,*) 'quad_trapezoid: size(x) != size(y)'
+            stop
+        end if
+
+		cum(1) = 0.
+		DO i = 2, SIZE(y)
+			cum(i) = cum(i-1) + (x(i)-x(i-1))*(y(i-1) + y(i))/2.
+		END DO
+
+	    integral = cum(SIZE(y))
+
+    RETURN
+    END SUBROUTINE xvec_yvec_cum_D
+
  END MODULE trapezoid_quad_m
--- RAYS_project/math_functions_lib/test_trapezoid.f90
+++ RAYS_project/math_functions_lib/test_trapezoid.f90
@@ -1,5 +1,5 @@
 PROGRAM test_trapezoid
-    USE trapezoid_quad_m, only : trapezoid_quad
+    USE trapezoid_quad_m, only : trapezoid_quad, trapezoid_quad_cumulative
 
     IMPLICIT NONE
 
@@ -12,72 +12,84 @@ PROGRAM test_trapezoid
     COMPLEX :: yC(npoints), integralC
     COMPLEX(kind = rkind) :: yDC(npoints), integralDC
 
-!     REAL :: y_cumulative(npoints)
-!     REAL(kind = rkind) :: y_cumulativeD(npoints)
-!     COMPLEX :: yC_cumulative(npoints)
-!     COMPLEX(kind = rkind) :: y_cumulativeDC(npoints)
-    
+    REAL :: cumulative(npoints)
+    REAL(kind = rkind) :: cumulativeD(npoints)
+
     real :: pi = 4.0*ATAN(1.0)
     REAL(kind = rkind) :: piD = 4.D0*ATAN(1.D0)
     integer :: i
-    
+
     h = 1.0/(npoints-1)
     hD = 1.0_rkind/(npoints-1)
-    
+
     DO i = 1, npoints
         x = h*(i-1)
         xvec(i) = x
         y(i) = x**3
         yC(i) = complex(cos(2.*pi*x), sin(2.*pi*x))
-        
+
         xD = hD*(i-1)
         xDvec(i) = xD
         yD(i) = xD**3
         yDC(i) = complex(cos(2.*piD*xD), sin(2.*piD*xD))
     END DO
 
-! fixed increment h, hD    
+! fixed increment h, hD
     write(*,*) ' '
     write(*,*) 'fixed increment: h = ', h
     write(*,*) 'y = ', y
 
     write(*,*) ' '
-    CALL trapezoid_quad(h, y, integral)    
+    CALL trapezoid_quad(h, y, integral)
     WRITE (*, *) ' single precision integral = ', integral
-    
-    CALL trapezoid_quad(hD, yD, integralD)    
+
+    CALL trapezoid_quad(hD, yD, integralD)
     WRITE (*, *) ' double precision integral = ', integralD
-    
-    CALL trapezoid_quad(h, yC, integralC)    
+
+    CALL trapezoid_quad(h, yC, integralC)
     WRITE (*, *) ' cmplx single precision integral = ', integralC
-    
-    CALL trapezoid_quad(hD, yDC, integralDC)    
+
+    CALL trapezoid_quad(hD, yDC, integralDC)
     WRITE (*, *) ' cmplx double precision integral = ', integralDC
 
+    CALL trapezoid_quad_cumulative(h, y, integral, cumulative)
+    WRITE (*, *) ' single precision integral = ', integral
+    WRITE (*, *) ' cumulative integral = ', cumulative
 
-! x vector   
+    CALL trapezoid_quad_cumulative(hD, yD, integralD, cumulativeD)
+    WRITE (*, *) ' double precision integral = ', integralD
+    WRITE (*, *) ' cumulative integral = ', cumulativeD
+
+
+! x vector
     write(*,*) ' '
     write(*,*) 'x vector'
     write(*,*) 'xvec = ', xvec
 
     write(*,*) ' '
-    CALL trapezoid_quad(xvec, y, integral)    
-    WRITE (*, *) ' single precision integral = ', integral
-    
-    CALL trapezoid_quad(xDvec, yD, integralD)    
-    WRITE (*, *) ' double precision integral = ', integralD
-    
-    CALL trapezoid_quad(xvec, yC, integralC)    
-    WRITE (*, *) ' cmplx single precision integral = ', integralC
-    
-    CALL trapezoid_quad(xDvec, yDC, integralDC)    
-    WRITE (*, *) ' cmplx double precision integral = ', integralDC
+    CALL trapezoid_quad(xvec, y, integral)
+    WRITE (*, *) ' xvec single precision integral = ', integral
 
+    CALL trapezoid_quad(xDvec, yD, integralD)
+    WRITE (*, *) ' xvec double precision integral = ', integralD
+
+    CALL trapezoid_quad(xvec, yC, integralC)
+    WRITE (*, *) ' xvec cmplx single precision integral = ', integralC
+
+    CALL trapezoid_quad(xDvec, yDC, integralDC)
+    WRITE (*, *) ' xvec cmplx double precision integral = ', integralDC
+
+    CALL trapezoid_quad_cumulative(xvec, y, integral, cumulative)
+    WRITE (*, *) ' xvec single precision integral = ', integral
+    WRITE (*, *) ' cumulative integral = ', cumulative
+
+    CALL trapezoid_quad_cumulative(xDvec, yD, integralD, cumulativeD)
+    WRITE (*, *) ' xvec double precision integral = ', integralD
+    WRITE (*, *) ' cumulative integral = ', cumulativeD
 
 
 !     write(*,*) ' '
 !     WRITE (*, *) 'integral = ', integral
 !     WRITE (*, *) 'cumulative integral = ', y_cumulative
-    
+
 END PROGRAM test_trapezoid
-    
