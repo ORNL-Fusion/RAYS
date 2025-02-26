bbbc4c34f58028e933a26e53b6f1635eb4625017
Donald Batchelor <batchelordb@ornl.gov>
2025-02-26 11:01:20 -0500

    Update RAYS_code/RAYS_P.f90 with changes due to postprocess_m

--- RAYS_project/RAYS_code/RAYS_P.f90
+++ RAYS_project/RAYS_code/RAYS_P.f90
@@ -1,7 +1,5 @@
  program rays_p
-!   simple_RAYS greatly simplified version of the MORAYS code to serve
-!   as test bed for ray tracing code development.  (DBB 5/2021)
-!
+
 !	This version links to the post_processing_lib library and (as of 3/23) uses
 !	the deposition_profiles_m module to calculate power deposition profiles after
 !	ray tracing completes.
@@ -9,9 +7,10 @@
 !   External procedures: initialize (initialize.f90), trace_rays (trace_rays.f90)
 !                        finalize (finalize.f90), all in RAYS_lib
 
-    use post_processing_m, only : post_process
+    use post_processing_m, only : initialize_post_processing_m, post_process_m, &
+                                & finalize_post_processing_m
 
-    implicit none 
+    implicit none
     logical :: read_input = .true.
 
 !****************************************************************************************
@@ -23,7 +22,7 @@
 
 !   Trace the rays.
     call trace_rays
-    
+
 !   Finish up, do post processing if any
     call finalize_run
 
@@ -33,13 +32,13 @@
 
 !   Read input file and initialize variables for post_processing_m only.
     read_input = .false.
-    call initialize_post_process_RAYS(read_input)
+    call initialize_post_processing_m(read_input)
 
 !   Do the post processing. Contained in module post_processing_m.f90
-    call post_process
+    call post_process_m
 
 !   Finish up, do post processing if any
-    call finalize_post_process_RAYS
-    
+    call finalize_post_processing_m
+
 
  end program rays_p
